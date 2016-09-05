8b64c50a59e0e92f5568942ca1290ed941dc7b1e
ChrisBarthol <Chris.Barthol@gmail.com>
2016-09-04 21:36:37 -0400

    select a max number

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,41 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, button, h1)
-import Html.Events exposing (onClick)
+import Html exposing (Html, div, text, button, h1, input)
+import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (..)
 import Html.App
 import Random exposing (..)
+import String exposing (..)
 
 --MODEL
 
 type alias Model =
   { dieFace : Int
+  , content : String
+  , maxNum : Int
   }
 
 init : (Model, Cmd Msg)
 init =
-  (Model 1, Cmd.none)
+  (Model 1 "Choose a Max" 10, Cmd.none)
 
 --MESSAGES
 
 type Msg
   = Roll
   | NewFace Int
+  | Max String
 
 --VIEW
 
 view : Model -> Html Msg
 view model =
-  div []
+  div [ class "text-center" ]
     [ h1 [] [ text (toString model.dieFace) ]
-    , button [ onClick Roll ] [ text "Roll" ]
+    , button [ onClick Roll ] [ text "Get New Number" ]
+    , div [] [input [ placeholder "Max Number", onInput Max ] []
+      , div [] [ text (model.content) ]
+      ]
     ]
 
 --UPDATE
@@ -37,10 +44,13 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Roll ->
-      (model, Random.generate NewFace (Random.int 1 6))
+      (model, Random.generate NewFace (Random.int 1 model.maxNum))
 
     NewFace newFace ->
-      (Model newFace, Cmd.none)
+      (Model newFace "Choose a Max" model.maxNum, Cmd.none)
+
+    Max number ->
+      (Model model.dieFace "Choose a Max" (toInt number |> Result.toMaybe |> Maybe.withDefault 10), Cmd.none)
 
 --SUBSCRIPTIONS
 
