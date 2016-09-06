86dd12cf3a3bd013f6b8f24b93056d38fe419aca
ChrisBarthol <Chris.Barthol@gmail.com>
2016-09-06 19:48:18 -0400

    cleanUp

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 require('./index.html');
 
 var Elm = require('./Main.elm');
-var mountNode = document.getElementById('main');
+
 
 // The third value on embed are the initial values for incomming ports into Elm
-var app = Elm.Main.embed(mountNode);
+Elm.Main.fullscreen();
